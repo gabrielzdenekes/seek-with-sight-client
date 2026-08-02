5a25191467813a2e42e735847785980e9b6a0977
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-08-02 13:36:10 +0200

    Add empty create product page

---
+++ src/pages/products/CreateProductPage.tsx
@@ -0,0 +1,5 @@
+const CreateProductPage = () => {
+    return <h1>Create product</h1>;
+};
+
+export default CreateProductPage;
--- src/pages/products/ProductDetailPage.tsx
+++
@@ -1,5 +0,0 @@
-const ProductDetailsPage = () => {
-    return <h1>ProductDetail</h1>;
-};
-
-export default ProductDetailsPage;
